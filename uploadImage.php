4f6d15754a851ad0d91cf3b12efd2fba7365b442
Xy <xyaranzaaa@gmail.com>
2020-08-21 17:22:36 +0800

    Updated headers

--- uploadImage.php
+++ uploadImage.php
@@ -6,9 +6,6 @@ use \Firebase\JWT\JWT;
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: POST");
-header("Access-Control-Max-Age: 3600");
-header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
-
 
 include_once 'database.php';
 include_once './classes/User.php';
